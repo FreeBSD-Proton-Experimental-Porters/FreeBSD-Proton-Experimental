a32a68d62bc9cb0e9d7392fef5eb08bc93046357
Vince <31942676+XaeroVincent@users.noreply.github.com>
2025-10-14 16:47:01 +0000

    Update Script for Proton Exp Python Scripts
    Update Script for Proton Exp Python Scripts

--- installsymlinks.sh
+++ installsymlinks.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# <<Important>> Be sure to install 'Proton Experimental' or 'Proton Experimental  [bleeding-edge]' tools in Linux Steam. 
+# <<Important>> Be sure to install 'Proton Experimental' or 'Proton Experimental [bleeding-edge]' tools in Linux Steam. 
 
-# For Proton Experimental 10 based DXVK + VKD3D-Proton builds
+# For Proton Experimental 10 Scripts and DXVK + VKD3D-Proton builds
+mv ~/.steam/steam/steamapps/common/Proton\ 10.0 ~/.steam/steam/steamapps/common/Proton\ 10.0.bak
+ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/ ~/.steam/steam/steamapps/common/Proton\ 10.0
+
 ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/i386-windows/* ~/.steam/steam/steamapps/common/Proton\ 10.0/files/lib/wine/dxvk/i386-windows
 ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/x86_64-windows/* ~/.steam/steam/steamapps/common/Proton\ 10.0/files/lib/wine/dxvk/x86_64-windows
 ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/i386-windows/* ~/.steam/steam/steamapps/common/Proton\ 10.0/files/lib/wine/vkd3d-proton/i386-windows
@@ -13,7 +16,7 @@ ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dx
 ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/i386-windows/* ~/WineGfx/vkd3d-proton-master/x32
 ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/x86_64-windows/* ~/WineGfx/vkd3d-proton-master/x64
 
-# For older Proton Experimental 9 based DXVK + VKD3D-Proton builds
+# For older Proton Experimental 9 DXVK + VKD3D-Proton builds
 #ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/* ~/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/files/lib/wine/dxvk
 #ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk/* ~/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/files/lib64/wine/dxvk
 #ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/* ~/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/files/lib/wine/vkd3d-proton

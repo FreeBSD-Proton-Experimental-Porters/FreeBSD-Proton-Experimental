6c54a910fc60c5ade668fc80cb8382a09eacc833
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-01-23 11:25:46 -0800

    Comment out optional symlinks
    Comment out optional symlinks for use outside of Steam

--- installprotonexp.sh
+++ installprotonexp.sh
@@ -3,10 +3,10 @@
 # <<Important>> Be sure to install 'Proton Experimental' or 'Proton Experimental [bleeding-edge]' tools in Linux Steam. 
 
 # For Proton Experimental 10 Scripts and DXVK + VKD3D-Proton builds
-ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/i386-windows/* ~/WineGfx/dxvk-master/x32
-ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/x86_64-windows/* ~/WineGfx/dxvk-master/x64
-ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/i386-windows/* ~/WineGfx/vkd3d-proton-master/x32
-ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/x86_64-windows/* ~/WineGfx/vkd3d-proton-master/x64
+#ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/i386-windows/* ~/WineGfx/dxvk-master/x32
+#ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/x86_64-windows/* ~/WineGfx/dxvk-master/x64
+#ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/i386-windows/* ~/WineGfx/vkd3d-proton-master/x32
+#ln -sf ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/x86_64-windows/* ~/WineGfx/vkd3d-proton-master/x64
 
 if ! [ -e ~/.steam/steam/steamapps/common/Proton\ 10.0.bak ]; then
     mv ~/.steam/steam/steamapps/common/Proton\ 10.0 ~/.steam/steam/steamapps/common/Proton\ 10.0.bak

f6259ece012b3641075428a78416afe238834bd4
XaeroVincent <tmp386@live.com>
2026-08-21 18:48:54 -0700

    Fix NUMA topology bug

--- emulators/proton-experimental/files/patch-0002_dlls_ntdll_unix_system.c
+++ emulators/proton-experimental/files/patch-0002_dlls_ntdll_unix_system.c
@@ -1,19 +1,11 @@
 --- dlls/ntdll/unix/system.c.orig	2026-08-20 17:48:16.269433000 -0700
 +++ dlls/ntdll/unix/system.c	2026-08-20 18:30:20.087816000 -0700
-@@ -1915,8 +1915,14 @@ static void init_logical_proc_info(void)
-         {
-             if (p->Relationship == RelationNumaNode || p->Relationship == RelationNumaNodeEx)
-             {
--                numa_info.ActiveProcessorsGroupAffinity[p->NumaNode.NodeNumber] = p->NumaNode.GroupMask;
--                ++numa_node_count;
-+                if (p->NumaNode.NodeNumber < ARRAY_SIZE(numa_info.ActiveProcessorsGroupAffinity))
-+                {
-+                    numa_info.ActiveProcessorsGroupAffinity[p->NumaNode.NodeNumber] = p->NumaNode.GroupMask;
-+                    ++numa_node_count;
-+                }
-+                else
-+                    FIXME( "NUMA node number %lu exceeds MAXIMUM_NUMA_NODE_COUNT (%u), ignoring.\n",
-+                           p->NumaNode.NodeNumber, (unsigned int)ARRAY_SIZE(numa_info.ActiveProcessorsGroupAffinity) );
-             }
-             p = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX *)((char *)p + p->Size);
-         }
+@@ -1742,7 +1749,7 @@ static NTSTATUS add_hwloc_numa_nodes(hwloc_topology_t 
+ 
+     for (obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_NUMANODE, 0); obj != NULL; obj = obj->next_cousin)
+     {
+-        if (!logical_proc_info_add_numa_node(obj->logical_index, hwloc_bitmap_to_ulong(obj->cpuset)))
++        if (!logical_proc_info_add_numa_node(hwloc_bitmap_to_ulong(obj->cpuset), obj->logical_index))
+             return STATUS_NO_MEMORY;
+     }
+     return STATUS_SUCCESS;

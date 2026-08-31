6d38574f646cadd9117d3784ad10e3f59352100d
XaeroVincent <tmp386@live.com>
2026-08-31 01:23:08 -0700

    Update D7VK and DXVK-Sarek

--- sarek/installsarek.sh
+++ sarek/installsarek.sh
@@ -4,8 +4,8 @@
 # https://github.com/pythonlover02/DXVK-Sarek
 # https://github.com/WinterSnowfall/d7vk
 
-SAREK_VER="1.12.0"
-D7VK_VER="2.1"
+SAREK_VER="1.13.0"
+D7VK_VER="2.2"
 DIR=$(dirname "$(realpath "$0")")
 PROTON_EXP_PATH="$HOME/.steam/steam/steamapps/common/Proton - Experimental"
 
@@ -18,15 +18,16 @@ setup() {
     rm -rf "${PROTON_EXP_PATH}/files/lib/wine/${name}"
     mkdir "${PROTON_EXP_PATH}/files/lib/wine/${name}"
     bsdtar -xf ${filename} --strip-components=1 -C "${PROTON_EXP_PATH}/files/lib/wine/${name}"
-    mv "${PROTON_EXP_PATH}/files/lib/wine/${name}/x32" "${PROTON_EXP_PATH}/files/lib/wine/${name}/i386-windows"
-    mv "${PROTON_EXP_PATH}/files/lib/wine/${name}/x64" "${PROTON_EXP_PATH}/files/lib/wine/${name}/x86_64-windows" 2> /dev/null
+    mv "${PROTON_EXP_PATH}/files/lib/wine/${name}/x32" "${PROTON_EXP_PATH}/files/lib/wine/${name}/i386-windows" 2> /dev/null
+    mv "${PROTON_EXP_PATH}/files/lib/wine/${name}/build/x32" "${PROTON_EXP_PATH}/files/lib/wine/${name}/i386-windows" 2> /dev/null
+    mv "${PROTON_EXP_PATH}/files/lib/wine/${name}/build/x64" "${PROTON_EXP_PATH}/files/lib/wine/${name}/x86_64-windows" 2> /dev/null
     rm ${filename}
 }
 
 cp "$DIR"/protonsarek.patch "${PROTON_EXP_PATH}" && cd "${PROTON_EXP_PATH}"
 patch -p1 < protonsarek.patch
 
-setup dxvk-sarek https://github.com/pythonlover02/DXVK-Sarek/releases/download/v${SAREK_VER} dxvk-sarek-dyasync-v${SAREK_VER}.tar.gz
+setup dxvk-sarek https://github.com/pythonlover02/DXVK-Sarek/releases/download/v${SAREK_VER} dxvk-sarek-${SAREK_VER}.tar.gz
 setup d7vk https://github.com/WinterSnowfall/d7vk/releases/download/v${D7VK_VER} d7vk-v${D7VK_VER}.zip
 
 echo "Installation complete."
